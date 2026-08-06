f7da8edd41bf12459c5b5b8ab74efdb455cb24f7
Hilman Alfiqri <hilmanalfiqri01@gmail.com>
2026-08-06 20:31:09 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Salin Project URL dan Publishable/Anon Key dari Supabase Project Settings > API.
-// PENTING: jangan pernah menaruh service_role key di file ini.
-window.LORHIL_CONFIG = {
-  SUPABASE_URL: "https://ancwuikyvddoeocliwwh.supabase.co",
-  SUPABASE_KEY: "sb_publishable_Y23m4Qzsu1NtJN48et-82g_BPtrp1oM"
-};
